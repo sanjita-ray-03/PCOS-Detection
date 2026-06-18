9dd0c8719dd84897f70ae26a6fe3cc59c18eb00c
Sanjita Ray <sanjitaray62@gmail.com>
2026-06-18 12:14:58 +0530

    Firebase env setup

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -1,9 +1,19 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("../../firebase-service-account.json");
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
+
+serviceAccount.private_key =
+  serviceAccount.private_key.replace(
+    /\\n/g,
+    "\n"
+  );
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(
+    serviceAccount
+  )
 });
 
 const db = admin.firestore();
